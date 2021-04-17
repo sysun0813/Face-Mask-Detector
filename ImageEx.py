ab9175357ac789280afb15fc6cfbef846ca59d61
sysun0813 <37135552+sysun0813@users.noreply.github.com>
2021-04-17 15:35:25 +0900

    Update ImageEx.py
    버튼을 누를 때마다 새 창을 띄우는 방식에서 한 창에 이미지를 띄우는 방식으로 변경

--- ImageEx.py
+++ ImageEx.py
@@ -1,7 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QLabel,QApplication, QWidget, QPushButton, QVBoxLayout
 
 from PyQt5.QtGui import QPixmap
 
@@ -13,8 +12,11 @@ class QtGUI(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.label1 = QLabel(self)  # 라벨 생성
+
         btn4 = QPushButton('Btn4', self)
         vbox = QVBoxLayout()
+        vbox.addWidget(self.label1)
         vbox.addWidget(btn4)
         self.setLayout(vbox)
         self.setWindowTitle('QPushButton')
@@ -23,16 +25,11 @@ class QtGUI(QWidget):
         btn4.clicked.connect(self.btn4Function)
 
     def btn4Function(self):
-        super().__init__()
         self.setWindowTitle("ImageEx")
-
-        label1 = QLabel(self)  # 라벨 생성
-
-        label1.move(10, 10)
 
         pixmap = QPixmap("C:/Users/eoshs/Desktop/디코 이모티콘/꿀벌.png")  # 이미지 관련 클래스 생성 및 이미지 불러오기
 
-        label1.setPixmap(pixmap)  # 이미지 관련 클래스와 라벨 연결
+        self.label1.setPixmap(pixmap)  # 이미지 관련 클래스와 라벨 연결
 
         self.resize(pixmap.width() + 20, pixmap.height() + 20)
 
