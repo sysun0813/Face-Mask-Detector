82659af2b5b5b4d15a0100c968eadf264208dcff
sysun0813 <37135552+sysun0813@users.noreply.github.com>
2021-04-17 14:54:28 +0900

    QPushButtion 예제 샘플

---
+++ QPushButton.py
@@ -0,0 +1,41 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        btn1 = QPushButton('&Button1', self)
+        btn1.setCheckable(True)
+        btn1.toggle()
+
+
+        btn2 = QPushButton(self)
+        btn2.setText('Button&2')
+
+
+        btn3 = QPushButton('Button3', self)
+        btn3.setEnabled(False)
+
+        btn4 = QPushButton('Btn4',self)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(btn1)
+        vbox.addWidget(btn2)
+        vbox.addWidget(btn3)
+        vbox.addWidget(btn4)
+
+        self.setLayout(vbox)
+        self.setWindowTitle('QPushButton')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
